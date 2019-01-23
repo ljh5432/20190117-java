bdd5732f8de790fd3de9bed4054c007688dc3838
지현 이 <ljh5432@gmail.com>
2019-01-23 16:23:04 +0900

    aaa

--- JavaWeb/src/com/java/Mybatis.java
+++ JavaWeb/src/com/java/Mybatis.java
@@ -16,6 +16,9 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+import net.sf.json.JSONObject;
+import net.sf.json.JSONSerializer;
+
 public class Mybatis {
 
 	public void main(HttpServletResponse response) {
@@ -23,8 +26,9 @@ public class Mybatis {
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("text/html;charset=UTF-8");
 		
-		String result = "";
+		//String result = "";
 		try {
+			PrintWriter pw = response.getWriter();
 			String resource = "mybatis-config.xml";
 			InputStream inputStream = Resources.getResourceAsStream(resource);
 			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
@@ -39,21 +43,31 @@ public class Mybatis {
 			  session.close();
 			}
 			
+			// JSON 파일 만들기
+			HashMap<String, Object> map = new HashMap<String, Object>();
+			map.put("data", list);
+			
+			JSONObject jo = new JSONObject(); 
+			jo = JSONObject.fromObject(JSONSerializer.toJSON(map));
+			pw.write(jo.toString());
+			
+			/*
 			for(int i = 0; i < list.size(); i++) {
 				HashMap<String, Object> map = (HashMap<String, Object>) list.get(i);
 				Set set =map.keySet();
 				Iterator it = set.iterator();
 				while(it.hasNext()) {
 					String key = it.next().toString();
-					result += key + " : " + map.get(key) + "\t";
+					pw.append(key + " : " + map.get(key) + "\t");
+					//result += key + " : " + map.get(key) + "\t";
 					//System.out.print(key + " : " + map.get(key) + "\t");
 				}
 				//System.out.println("");
-				result += "<br>";
+				//result += "<br>";
+				pw.append("<br>");
 			}
-			
-			PrintWriter pw = response.getWriter();
-			pw.write(result);
+			*/
+			//pw.write(result);
 			
 		} catch (IOException e) {
 			e.printStackTrace();
